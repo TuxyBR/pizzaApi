38237f2218f58dba4ddc0cc3038199f7709e0cd7
TuxyBR <rafinhalm.lm@gmail.com>
2026-01-06 09:59:05 -0400

    GET pizzas

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"github.com/TuxyBR/pizzaApi/models"
 	"github.com/gin-gonic/gin"
 )
 
+var pizzas = []models.Pizza{
+	{ID: 1, Name: "Toscana", Price: 49.5},
+	{ID: 2, Name: "Marguerita", Price: 79.5},
+	{ID: 3, Name: "Atum com queijo", Price: 69.5},
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/pizzas", getPizzas)
 	r.Run()
+}
+
+func getPizzas(c *gin.Context) {
+	c.JSON(200, gin.H{"pizzas": pizzas})
 }
