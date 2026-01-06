f23d0ee1eabead015448c6d2d21494f36b48ade1
TuxyBR <rafinhalm.lm@gmail.com>
2026-01-06 09:57:42 -0400

    move pizza struct to models package

--- main.go
+++ main.go
@@ -4,12 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Pizza struct {
-	ID    int
-	name  string
-	price float64
-}
-
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
