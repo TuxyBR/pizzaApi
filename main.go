94d34a8e49724e811d4e2920658d8ad661fae2a9
TuxyBR <rafinhalm.lm@gmail.com>
2026-01-06 10:00:03 -0400

    GET pizza by id param

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/TuxyBR/pizzaApi/models"
 	"github.com/gin-gonic/gin"
 )
@@ -14,12 +16,31 @@ var pizzas = []models.Pizza{
 func main() {
 	r := gin.Default()
 	r.GET("/pizzas", getPizzas)
+	r.GET("/pizzas/:id", getPizzaId)
 	r.POST("/pizzas", postPizzas)
 	r.Run()
 }
 
 func getPizzas(c *gin.Context) {
 	c.JSON(200, gin.H{"pizzas": pizzas})
+}
+
+func getPizzaId(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"erro": err.Error(),
+		})
+		return
+	}
+	for _, piz := range pizzas {
+		if piz.ID == id {
+			c.JSON(200, gin.H{"pizza": piz})
+			return
+		}
+	}
+	c.JSON(404, gin.H{"message": "pizza not found"})
 }
 
 func postPizzas(c *gin.Context) {
